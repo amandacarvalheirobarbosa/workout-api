c6792f7f335c55de6cc3a79b2d27e3f9a9bee208
Amanda Carvalheiro Barbosa <acarvalheirobarbosa@gmail.com>
2024-04-26 09:22:00 -0300

    fix: configs

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter, status
+from uuid import uuid4
+from fastapi import APIRouter, Body, status
+from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
+from workout_api.categorias.models import CategoriaModel
+
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
 @router.post(
     '/',
     summary='Criar nova categoria',
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
+    response_model=CategoriaOut
 )
-async def post():
+async def post(
+    db_session: DatabaseDependency,
+    categoria_in: CategoriaIn = Body(...)
+) -> CategoriaOut:
+    categoria_out = CategoriaOut(id=uuid4(), **categoria_in.dict())
+    categoria_model = CategoriaModel(**categoria_out.model_dump())
+    # breakpoint()
     pass
--- workout_api/categorias/schemas.py
+++ workout_api/categorias/schemas.py
@@ -1,7 +1,14 @@
 from typing import Annotated
-from pydantic import Field
-from workout_api.contrib.schemas import BaseSchema
+from pydantic import UUID4, Field
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
 class Categoria(BaseSchema):
     nome: Annotated[str, Field(description='Nome da Categoria', example='Scale', max_length=10)]
+
+
+class CategoriaIn(Categoria):
+    pass
+
+class CategoriaOut(Categoria):
+    id: Annotated[UUID4, Field(description='Identificador da categoria')]
