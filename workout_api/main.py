bbde3a7110f0cafc5bad1b68769a3cd3dcf8fbfc
Amanda Carvalheiro Barbosa <acarvalheirobarbosa@gmail.com>
2024-04-26 08:52:33 -0300

    feat: configurações e correções de rotas

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
+app.include_router(api_router)
 
 # Comentar daqui pra baixo se usar o makefile
 if __name__ == '__main__':
