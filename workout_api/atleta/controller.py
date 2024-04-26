82f776325ffdf1d4eaf5be63aa45164bdd83f377
Amanda Carvalheiro Barbosa <acarvalheirobarbosa@gmail.com>
2024-04-26 09:10:34 -0300

    fix: padronização

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Body, status
+from workout_api.atleta.schemas import AtletaIn
+
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
@@ -7,5 +10,7 @@ router = APIRouter()
     summary='Criar novo atleta',
     status_code=status.HTTP_201_CREATED
 )
-async def post():
+async def post(
+    de_session: DatabaseDependency,
+    atleta_in: AtletaIn = Body(...)):
     pass
--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from pydantic import Field, PositiveFloat
-from workout_api.contrib.schemas import BaseSchema
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
 class Atleta(BaseSchema):
@@ -10,3 +10,11 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do Atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do Atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do Atleta', example='M', max_length=1)]
+
+
+class AtletaIn(Atleta):
+    pass
+
+
+class AtletaOut(Atleta, OutMixin):
+    pass
